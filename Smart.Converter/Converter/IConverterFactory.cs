0c310755443f428ebd8a183c26d5d11a86137207
usausa <machi.pon@gmail.com>
2026-09-23 15:41:56 +0900

    Update

--- Smart.Converter/Converter/IConverterFactory.cs
+++ Smart.Converter/Converter/IConverterFactory.cs
@@ -8,7 +8,6 @@ public interface IConverterFactory
     [RequiresUnreferencedCode("Converter factories use reflection to discover types at runtime.")]
     Func<object, object?>? GetConverter(IObjectConverter context, Type sourceType, Type targetType);
 
-    // The returned converter returns ConvertFailure.Value when the value cannot be converted
     [RequiresDynamicCode("Converter factories use MakeGenericType/MakeGenericMethod at runtime.")]
     [RequiresUnreferencedCode("Converter factories use reflection to discover types at runtime.")]
     Func<object, object?>? GetTryConverter(IObjectConverter context, Type sourceType, Type targetType) => null;
